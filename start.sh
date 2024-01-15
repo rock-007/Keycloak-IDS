e57cfea9d6f7f41e4174e60333cedbbb9ebc6be1
Umair Ashraf <59394025+rock-007@users.noreply.github.com>
2024-01-15 22:58:54 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download and run the install-java.sh script
-wget https://raw.githubusercontent.com/chrishantha/install-java/master/install-java.sh
-chmod +x install-java.sh
-sudo ./install-java.sh -f jdk-11.0.12_linux-x64_bin.tar.gz -p /opt/java
-# Set the JAVA variable to the path of the Java executable
-export JAVA=/opt/java/jdk-11.0.12/bin/java
+# wget https://raw.githubusercontent.com/chrishantha/install-java/master/install-java.sh
+# chmod +x install-java.sh
+# sudo ./install-java.sh -f jdk-11.0.12_linux-x64_bin.tar.gz -p /opt/java
+# # Set the JAVA variable to the path of the Java executable
+# export JAVA=/opt/java/jdk-11.0.12/bin/java
 
 # Start nginx in the background
 nginx -g 'daemon off;' &
