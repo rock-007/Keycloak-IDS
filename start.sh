1efff49ee40f7c279f4d6c4d23b3322eaed67d73
Umair Ashraf <59394025+rock-007@users.noreply.github.com>
2024-01-15 22:24:53 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 # Start nginx in the background
 nginx -g 'daemon off;' &
 # Start keycloak in the background
-/opt/bitnami/keycloak/run.sh &
+/opt/keycloak/bin/kc.sh &
 # Wait for both processes to finish
 wait
